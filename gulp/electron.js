38ea1ead6bc9a0abf4b0d5c74a6c5497a7b742bf
Yi Kai <beliefs111@gmail.com>
2016-02-17 11:40:12 +0800

    add gulp tasks for live reload, add build and rebuild scripts

---
+++ gulp/electron.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var gulp = require('gulp');
+var path = require('path');
+var conf = require('./conf');
+
+//Loading all gulp plugins
+var $ = require('gulp-load-plugins')();
+
+var electron = require('electron-connect').server.create();
+
+gulp.task('electron-start', function () {
+
+    // Start browser process
+    electron.start();
+
+    // Restart browser process
+    gulp.watch('app.js', electron.restart);
+
+    // Reload renderer process
+    gulp.watch(['index.js', 'index.html'], electron.reload);
+});
